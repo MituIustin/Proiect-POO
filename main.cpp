14f67940e9f03221e95f80b29b025b8436cdbb69
IustinMitu <iustin-aurelian.mitu@s.unibuc.ro>
2023-03-02 18:44:50 +0200

    inceput

--- main.cpp
+++ main.cpp
@@ -1,61 +1,205 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <chrono>
-#include <thread>
+#include <cstdlib>
+#include <time.h>
+#include <string>
+#include <vector>
 
-#ifdef __linux__
-#include <X11/Xlib.h>
-#endif
-
-class SomeClass {
+class Joc{
+private:
 public:
-    explicit SomeClass(int) {}
+    bool Conditie_Speciala = false;
+    void Start()
+    {
+
+    }
+
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
+class Carte{
+private:
+    int valoare;
+    std::string culoare;
+    std::string simbol;
+public:
 
-int main() {
-    #ifdef __linux__
-    XInitThreads();
-    #endif
+    Carte()
+    {
+        valoare = rand()%13+1;
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
+        int nr_culoare = rand()%2;
+        if(nr_culoare == 0) culoare = "rosu";
+        else culoare = "negru";
 
-    sf::RenderWindow window;
-    // NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:30
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    window.setVerticalSyncEnabled(true);
-    //window.setFramerateLimit(60);
+        if(nr_culoare == 0)
+        {
+            int nr_simbol = rand()%2;
+            if(nr_simbol == 0) simbol = "romb" ;
+            else simbol = "inima";
+        }
+        else
+        {
+            int nr_simbol = rand()%2;
+            if(nr_simbol == 0) simbol = "trefla" ;
+            else simbol = "inima";
+        }
 
-    while(window.isOpen()) {
-        sf::Event e;
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                break;
-            default:
+    }
+    Carte(int val, std::string cul, std::string sim)
+    {
+        valoare = val;
+        culoare = cul;
+        simbol = sim;
+    }
+
+    int CarteSpeciala( Carte c)
+    {
+        if(c.GetValoare() == 1) return 1;
+        if(c.GetValoare() == 7) return 7;
+        if(c.GetValoare() == 11) return 11;
+        return 0;
+    }
+
+    void AfisareCarte()
+    {
+        std::cout<<valoare<<" "<<simbol<<" "<<culoare<<"\n";
+    }
+
+    int GetValoare()
+    {
+        return valoare;
+    }
+    std::string GetSimbol()
+    {
+        return simbol;
+    }
+    std::string GetCuloare()
+    {
+        return culoare;
+    }
+
+    ~Carte(){}
+};
+
+class Pachet{
+private:
+    std::vector<Carte> pachet;
+    int nr_carti = 0;
+public:
+    void AdaugareCarte(Carte c)
+    {
+        pachet.push_back(c);
+        nr_carti++;
+    }
+    void AmestecarePachet()
+    {
+        std::random_shuffle(pachet.begin(),pachet.end());
+    }
+    void AfisarePachet()
+    {
+        for (auto it = pachet.begin(); it != pachet.end(); ++it)
+        {
+            it->AfisareCarte();
+        }
+        std::cout<<"\n";
+    }
+    Carte TragCarte()
+    {
+        nr_carti--;
+        Carte c = pachet[nr_carti];
+        pachet.pop_back();
+        return c;
+    }
+    ~Pachet(){}
+};
+
+class Jucator{
+private:
+    std::vector<Carte> mana;
+    int nr_carti=0;
+public:
+
+
+    void AfisareMana()
+    {
+        for (auto it = mana.begin(); it != mana.end(); ++it)
+        {
+            it->AfisareCarte();
+        }
+        std::cout<<"\n";
+    }
+
+    void EliminareCarte( Carte c)
+    {
+        for (auto it = mana.begin(); it != mana.end(); ++it)
+        {
+            if(it->GetValoare() == c.GetValoare() &&
+                it->GetCuloare() == c.GetCuloare() &&
+                it->GetSimbol() == c.GetSimbol())
+            {
+                mana.erase(it);
+                nr_carti--;
                 break;
             }
         }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
-        window.clear();
-        window.display();
+    }
+    void TragCarte(Carte c)
+    {
+        nr_carti++;
+        mana.push_back(c);
     }
 
-    return 0;
+    ~Jucator(){}
+};
+
+
+void ConstruirePachet(Pachet &p)
+{
+    for(int i = 1; i<=13; i++)
+    {
+        Carte carte1( i , "rosu" , "romb");
+        Carte carte2( i , "rosu" , "inima");
+        Carte carte3( i , "negru" , "inima");
+        Carte carte4( i , "nergu" , "trefla");
+        p.AdaugareCarte(carte1);
+        p.AdaugareCarte(carte2);
+        p.AdaugareCarte(carte3);
+        p.AdaugareCarte(carte4);
+    }
+}
+
+int main()
+{
+    srand(time(0));
+
+    int nr_jucatori = rand() %3 +2;
+    std::cout<<nr_jucatori<<"\n";
+
+
+    Pachet p;
+    ConstruirePachet(p);
+    p.AmestecarePachet();
+    p.AfisarePachet();
+
+    std::vector<Jucator> jucatori;
+
+    for(int i=0; i<nr_jucatori; i++)
+    {
+        Jucator juc;
+
+        for(int j=0; j<5; j++)
+        {
+            juc.TragCarte(p.TragCarte());
+        }
+        jucatori.push_back(juc);
+    }
+
+    p.AfisarePachet();
+
+    for(int i=0; i<nr_jucatori; i++)
+    {
+        jucatori[i].AfisareMana();
+    }
+
+
 }
