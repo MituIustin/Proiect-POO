ce18319b4d92294ded6c5558decb1095140c3f06
IustinMitu <iustin-aurelian.mitu@s.unibuc.ro>
2023-03-05 14:53:51 +0200

    Prima parte a proiectului

--- main.cpp
+++ main.cpp
@@ -5,18 +5,9 @@
 #include <string>
 #include <vector>
 
-class Joc{
-private:
-public:
-    bool Conditie_Speciala = false;
-    void Start()
-    {
-
-    }
-
-};
 
 class Carte{
+
 private:
     int valoare;
     std::string culoare;
@@ -51,20 +42,19 @@ public:
         culoare = cul;
         simbol = sim;
     }
-
-    int CarteSpeciala( Carte c)
+    int CarteSpeciala()
     {
-        if(c.GetValoare() == 1) return 1;
-        if(c.GetValoare() == 7) return 7;
-        if(c.GetValoare() == 11) return 11;
+        //if(valoare == 1) return 1;
+        //if(valoare == 7) return 7;
+        //if(valoare == 11) return 11;
         return 0;
     }
-
-    void AfisareCarte()
+    friend std::ostream& operator<<(std::ostream& os, const Carte& ct)          //supraincarcare operator<<
     {
-        std::cout<<valoare<<" "<<simbol<<" "<<culoare<<"\n";
+        os<<ct.valoare<<" "<<ct.simbol<<" "<<ct.culoare<<"\n";
+        return os;
     }
-
+    //supraincarcare operator >> (as avea nevoie)
     int GetValoare()
     {
         return valoare;
@@ -72,13 +62,66 @@ public:
     std::string GetSimbol()
     {
         return simbol;
-    }
+    }       //getter
     std::string GetCuloare()
     {
         return culoare;
-    }
+    }       //getter
 
     ~Carte(){}
+};
+
+class Jucator{
+private:
+    std::vector<Carte> mana;
+    int nr_carti=0;
+public:
+    void EliminareCarte( Carte c)
+    {
+        for (auto it = mana.begin(); it != mana.end(); ++it)
+        {
+            if(it->GetValoare() == c.GetValoare() &&
+               it->GetCuloare() == c.GetCuloare() &&
+               it->GetSimbol() == c.GetSimbol())
+            {
+                mana.erase(it);
+                nr_carti--;
+                break;
+            }
+        }
+    }
+
+    void AfisareMana()                              //functia asta va fi stearsa probabil, am folosit-o pt debugging
+    {
+        for (auto it = mana.begin(); it != mana.end(); ++it)
+        {
+            std::cout<<*it;
+        }
+        std::cout<<"\n";
+    }
+
+
+    void TragCarte(Carte c)
+    {
+        nr_carti++;
+        mana.push_back(c);
+    }
+
+    void PuneCarte(Carte c)
+    {
+        EliminareCarte(c);
+    }
+    int Getnr_carti()                   //getter
+    {
+        return nr_carti;
+    }
+
+    std::vector<Carte> Getmana()
+    {
+        return mana;
+    }
+
+    ~Jucator(){}
 };
 
 class Pachet{
@@ -86,6 +129,32 @@ private:
     std::vector<Carte> pachet;
     int nr_carti = 0;
 public:
+    Pachet ()                                       //constructor
+    {
+        std::vector<Carte> _pachet;
+        pachet = _pachet;
+        int _nr_carti = 0;
+        nr_carti = _nr_carti;
+    }
+    Pachet& operator=(const Pachet& other){             //operator de mutare
+        pachet = std::move(other.pachet);
+        nr_carti = std::move(other.nr_carti);
+        return *this;
+    }
+    void EliminareCarte(Carte c)
+    {
+        for (auto it = pachet.begin(); it != pachet.end(); ++it)
+        {
+            if(it->GetValoare() == c.GetValoare() &&
+               it->GetCuloare() == c.GetCuloare() &&
+               it->GetSimbol() == c.GetSimbol())
+            {
+                pachet.erase(it);
+                nr_carti--;
+                break;
+            }
+        }
+    }
     void AdaugareCarte(Carte c)
     {
         pachet.push_back(c);
@@ -99,9 +168,13 @@ public:
     {
         for (auto it = pachet.begin(); it != pachet.end(); ++it)
         {
-            it->AfisareCarte();
+            std::cout<<*it;
         }
         std::cout<<"\n";
+    }
+    int GetNr()
+    {
+        return nr_carti;
     }
     Carte TragCarte()
     {
@@ -110,96 +183,205 @@ public:
         pachet.pop_back();
         return c;
     }
+    Carte UltimaCarte ()
+    {
+        return *pachet.end();
+    }
+
+
     ~Pachet(){}
 };
 
-class Jucator{
+
+class Joc{
 private:
-    std::vector<Carte> mana;
-    int nr_carti=0;
-public:
+    int nr_jucatori;
+    int jucator_prezent = 0;        //"a cui este randul"
+    Pachet p;                       //pachetul din care se trag carti
+    Pachet pjos;                    //pachetul de jos unde se pun carti
+    std::vector<Jucator> jucatori;          //vetorul de jucatori
+    bool conditie_speciala = false;
+    bool joc_activ = true;
 
-
-    void AfisareMana()
+    void random_generator()
     {
-        for (auto it = mana.begin(); it != mana.end(); ++it)
-        {
-            it->AfisareCarte();
-        }
-        std::cout<<"\n";
+        srand(time(0));
     }
-
-    void EliminareCarte( Carte c)
+    void creare_jucatori()                      //nr jucatorilor variaza intre 2 si 4
     {
-        for (auto it = mana.begin(); it != mana.end(); ++it)
+        int _nr_jucatori = rand() %3 +2;
+        nr_jucatori = _nr_jucatori;
+        std::cout<<_nr_jucatori<<"\n";
+    }
+    void ConstruirePachet(Pachet &pr)
+    {
+        for(int i = 1; i<=13; i++)
         {
-            if(it->GetValoare() == c.GetValoare() &&
-                it->GetCuloare() == c.GetCuloare() &&
-                it->GetSimbol() == c.GetSimbol())
+            Carte carte1( i , "rosu" , "romb");
+            Carte carte2( i , "rosu" , "inima");
+            Carte carte3( i , "negru" , "inima");
+            Carte carte4( i , "nergu" , "trefla");
+            pr.AdaugareCarte(carte1);
+            pr.AdaugareCarte(carte2);
+            pr.AdaugareCarte(carte3);
+            pr.AdaugareCarte(carte4);
+        }
+    }
+    void creare_pachet()                    //aici se creaza un pachet cu toate cartile, iar apoi se muta cu operator=
+    {
+        Pachet _p;
+        ConstruirePachet(_p);
+        _p.AmestecarePachet();
+        _p.AfisarePachet();
+        p = _p;                         // constructor de copiere cu operator=
+    }
+    void impartire_carti()                  //functia imparte cate 5 carti la fiecare jucator
+    {
+        for(int i=0; i<nr_jucatori; i++)
+        {
+            Jucator juc;
+
+            for(int j=0; j<5; j++)
             {
-                mana.erase(it);
-                nr_carti--;
-                break;
+                juc.TragCarte(p.TragCarte());
+            }
+            jucatori.push_back(juc);
+        }
+    }
+    void creare_pachet_jos()            //metoda apelata o singura data la incepera jocului
+    {
+        pjos.AdaugareCarte(p.TragCarte());
+    }
+    bool verificare( Carte c)           //verific daca poate fi pusa jos cartea c
+    {
+        if((c.GetCuloare()==pjos.UltimaCarte().GetCuloare()
+        && c.GetSimbol()==pjos.UltimaCarte().GetSimbol())
+        || c.GetValoare()==pjos.UltimaCarte().GetValoare())
+        {
+            return true;
+        }
+        return false;
+    }
+    void joc_efectiv()
+    {
+        while(joc_activ)
+        {
+            if(jucator_prezent==nr_jucatori || jucator_prezent==0)
+            {
+                std::cout<<"Cartile tale: \n";
+                jucatori[0].AfisareMana();
+                std::cout<<"Cartea de jos este: "<<pjos.UltimaCarte();
+                std::cout<<"Ce carte dai jos? \n";
+                int optiune;
+                std::cin>>optiune;                                  // ?????????????
+                std::cout<<"ok";
+                /*
+                 *
+                 *
+                 *
+                 *
+                 *
+                 */
+                if(jucatori[0].Getnr_carti()==0)
+                {
+                    joc_activ = false;
+                }
+
+                jucator_prezent = 1;
+            }
+            else
+            {
+                if(!conditie_speciala)                      //aici botul contra care joci alege prima carte pe care o poate pune jos, si apoi o pune
+                {
+                    bool dat_jos=false;
+                    for (auto it = jucatori[jucator_prezent].Getmana().begin(); it != jucatori[jucator_prezent].Getmana().end(); ++it)
+                    {
+                        if(verificare(*it))
+                        {
+                            jucatori[jucator_prezent].EliminareCarte(*it);
+                            pjos.AdaugareCarte(*it);
+                            dat_jos=true;
+                            int val_carte = it->CarteSpeciala();
+                            if(val_carte!=0) conditie_speciala =true;           //aici verific daca (,) carte este 1, 7 sau 11
+                            else conditie_speciala = false;
+                            break;
+                        }
+                    }
+                    if(dat_jos == false)                        //daca jucatorul nu are ce sa dea jos, va trage o carte
+                    {
+                        jucatori[jucator_prezent].TragCarte(p.TragCarte());
+                    }
+                }
+                /*
+                 * partea de implementare cand exista o conditie speciala inca nu este implementata
+                 *
+                 *
+                 *
+                else
+                {
+                    int val_carte = pjos.UltimaCarte().CarteSpeciala();
+                    switch (val_carte) {
+                        case 1:
+                            jucator_prezent++;
+                            break;
+                        case 7:
+
+
+                    }
+                    bool dat_jos=false;
+                    for (auto it = jucatori[jucator_prezent].mana.begin(); it != jucatori[jucator_prezent].mana.end(); ++it)
+                    {
+                        if(verificare(*it))
+                        {
+                            jucatori[jucator_prezent].EliminareCarte(*it);
+                            pjos.AdaugareCarte(*it);
+                            dat_jos=true;
+                            break;
+                        }
+                    }
+                    if(dat_jos== false)
+                    {
+                        jucatori[jucator_prezent].TragCarte(p.TragCarte());
+                    }
+                }
+                */
+                jucator_prezent ++;
+                if(jucatori[jucator_prezent].Getnr_carti()==0)
+                {
+                    joc_activ = false;
+                }
+            }
+            if(p.GetNr()==0)                                        //aici verific daca pachetul din care se trag carti
+            {                                                       //mai are carti, in caz negativ , se pastreaza doar
+                Carte ult = pjos.UltimaCarte();                     //ultima carte din pachetul de jos , restul se
+                p = pjos;                                           //muta cu operatorul= in pachetul din care se trag
+                p.AmestecarePachet();                               //carti , iar apoi se amesteca
+                pjos.AdaugareCarte(ult);
+                p.EliminareCarte(ult);
             }
         }
+
     }
-    void TragCarte(Carte c)
+public:
+
+    void Start()
     {
-        nr_carti++;
-        mana.push_back(c);
+        random_generator();
+        creare_jucatori();
+        creare_pachet();
+        impartire_carti();
+        creare_pachet_jos();
+        joc_efectiv();
     }
 
-    ~Jucator(){}
 };
 
 
-void ConstruirePachet(Pachet &p)
-{
-    for(int i = 1; i<=13; i++)
-    {
-        Carte carte1( i , "rosu" , "romb");
-        Carte carte2( i , "rosu" , "inima");
-        Carte carte3( i , "negru" , "inima");
-        Carte carte4( i , "nergu" , "trefla");
-        p.AdaugareCarte(carte1);
-        p.AdaugareCarte(carte2);
-        p.AdaugareCarte(carte3);
-        p.AdaugareCarte(carte4);
-    }
-}
 
 int main()
 {
-    srand(time(0));
-
-    int nr_jucatori = rand() %3 +2;
-    std::cout<<nr_jucatori<<"\n";
-
-
-    Pachet p;
-    ConstruirePachet(p);
-    p.AmestecarePachet();
-    p.AfisarePachet();
-
-    std::vector<Jucator> jucatori;
-
-    for(int i=0; i<nr_jucatori; i++)
-    {
-        Jucator juc;
-
-        for(int j=0; j<5; j++)
-        {
-            juc.TragCarte(p.TragCarte());
-        }
-        jucatori.push_back(juc);
-    }
-
-    p.AfisarePachet();
-
-    for(int i=0; i<nr_jucatori; i++)
-    {
-        jucatori[i].AfisareMana();
-    }
-
+    Joc joc_nou;
+    joc_nou.Start();
+    std::cout<<"Joc terminat";
 
 }
